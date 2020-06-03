75e3532857ed14f71329a79ca2a24af8e9414bea
Joel <jmarcustaylor@gmail.com>
2020-06-03 13:36:17 +0100

    created aI follow and sandbox --give that player a gun--

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 10f;
+    
+
+    NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+    }
+    void Update()
+    {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+
+        if (isProvoked)
+        {
+            EngageTarget();
+        }
+        else if (distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+
+           
+        }
+ 
+    }
+
+    private void EngageTarget()
+    {
+        if (distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+             ChaseTarget();
+        }
+        if (distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
+    void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    void AttackTarget()
+    {
+        print("boy you dead");
+    }
+
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(50, 0, 0);
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+}
