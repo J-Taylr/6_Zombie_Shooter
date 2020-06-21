a2241a8786b9b5e12f34d74c7460ad92e992402b
Joel <jmarcustaylor@gmail.com>
2020-06-21 13:31:41 +0100

    weapon zoom script added and implemented -- Weapon Zoom - Mouse Sensitivity --

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [SerializeField] Camera fpsCamera;
+    [SerializeField] float fovOut;
+    [SerializeField] float fovIn;
+
+
+    private void Start()
+    {
+        fpsCamera.fieldOfView = fovOut;
+    }
+
+    private void Update()
+    {
+        PlayerZoom();
+    }
+
+    private void PlayerZoom()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            fpsCamera.fieldOfView = fovIn;
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            fpsCamera.fieldOfView = fovOut;
+        }
+    }
+}
